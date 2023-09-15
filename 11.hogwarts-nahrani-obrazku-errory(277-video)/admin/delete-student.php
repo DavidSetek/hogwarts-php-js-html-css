0e5853fe75dbb35e01e7ed7664a075c05c4ee387
DavidSetek <98630991+DavidSetek@users.noreply.github.com>
2023-09-15 16:47:30 +0200

    Add files via upload

---
+++ 11.hogwarts-nahrani-obrazku-errory(277-video)/admin/delete-student.php
@@ -0,0 +1,62 @@
+<?php
+
+// require "../assets/database.php";
+// require "../assets/zak.php";
+// require "../assets/auth.php";
+// require "../assets/url.php";
+require "../classes/Database.php";
+require "../classes/Url.php";
+require "../classes/Student.php";
+require "../classes/Auth.php";
+
+session_start();
+
+if ( !Auth::isLoggedIn() ){
+    die("Nepovolený přístup");
+}
+
+// $connection = connectionDB();
+$database = new Database();
+$connection = $database->connectionDB();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if(Student::deleteStudent($connection, $_GET["id"])){
+        Url::redirectUrl("/skola-projekt/admin/students.php");
+    };
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../query/header-query.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <script src="https://kit.fontawesome.com/0fe3234472.js" crossorigin="anonymous"></script>
+    <title>Smazat žáka</title>
+</head>
+<body>
+    <?php require "../assets/admin-header.php"; ?>
+
+    <main>
+        <section class="delete-form">
+            <form method="POST">
+                <p>Jste si jisti, že chcete tohoto žáka smazat?</p>
+                <button>Smazat</button>
+                <a href="one-student.php?id=<?= $_GET['id'] ?>">Zrušit</a>
+            </form>
+        </section>
+    </main>
+   
+    <?php require "../assets/footer.php"; ?>
+    <script src="../js/header.js"></script>
+</body>
+</html>
+
+
+
+
---
+++ 11.hogwarts-nahrani-obrazku-errory(277-video)/admin/one-student.php
@@ -0,0 +1,70 @@
+<?php
+
+    // require "../assets/database.php";
+    // require "../assets/zak.php";
+    // require "../assets/auth.php";
+    require "../classes/Database.php";
+    require "../classes/Student.php";
+    require "../classes/Auth.php";
+
+    session_start();
+
+    if ( !Auth::isLoggedIn() ){
+        die("Nepovolený přístup");
+    }
+
+    // $connection = connectionDB();
+    $database = new Database();
+    $connection = $database->connectionDB();
+
+    if ( isset($_GET["id"]) and is_numeric($_GET["id"]) ) {
+        $students = Student::getStudent($connection, $_GET["id"]);
+    } else {
+        $students = null;
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../query/header-query.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <script src="https://kit.fontawesome.com/0fe3234472.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <?php require "../assets/admin-header.php"; ?>
+
+    <main>
+        <section class="main-heading">
+            <h1>Informace o žákovi</h1>
+        </section>
+
+         <section>
+            <?php if ($students === null): ?>
+                <p>Žák nenalezen</p>
+            <?php else: ?>
+                <h2><?= htmlspecialchars($students["first_name"]). " " .htmlspecialchars($students["second_name"]) ?></h2>
+                <p>Věk: <?= htmlspecialchars($students["age"]) ?></p>
+                <p>Dodatečné informace: <?= htmlspecialchars($students["life"]) ?></p>
+                <p>Kolej: <?= htmlspecialchars($students["college"]) ?></p>
+            <?php endif ?>    
+        </section>
+
+        <section class="buttons">
+                <a href="edit-student.php?id=<?= $students['id'] ?>">Editovat</a>
+                <a href="delete-student.php?id=<?= $students['id'] ?>">Vymazat</a>
+        </section>        
+
+    </main>
+
+    <?php require "../assets/footer.php"; ?>
+    <script src="../js/header.js"></script>
+</body>
+</html>
